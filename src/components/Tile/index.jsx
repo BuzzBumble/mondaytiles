f7d3f275946b26e816585ec8ba37a5e7794a9cec
Estevan Sinarta <estevan_sinarta@yahoo.ca>
2021-04-23 20:37:51 -0700

    Coloured status tiles using conditional classes

--- src/components/Tile/index.jsx
+++ src/components/Tile/index.jsx
@@ -1,5 +1,7 @@
 import './Tile.css';
 import PropTypes from 'prop-types';
+import classNames from "classnames";
+import { shortName } from 'helpers/util';
 
 // Tile Component
 // Basic display component for a Tile
@@ -9,10 +11,18 @@ const Tile = props => {
   const roundedWeight = Math.round(props.weight * 100000) / 1000;
 
   return (
-    <div style={props.style} className="tile" onClick={props.onClick}>
-      <p>
-        {props.name} ({props.value})
-      </p>
+    <div 
+      style={props.style} 
+      className={classNames({
+        'tile': true,
+        'done': props.name == 'Done',
+        'working': props.name == shortName('Working on it'),
+        'stuck': props.name == 'Stuck'
+      })}
+      onClick={props.onClick}>
+      <p className='main-title'>Name: {props.name}</p>
+      <p>Value: {props.value}</p>
+      <p>Weight: {roundedWeight}</p>
     </div>
   );
 };
