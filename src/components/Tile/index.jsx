d40540621163fd609177f618405266802c601d16
Estevan Sinarta <estevan_sinarta@yahoo.ca>
2021-04-24 23:51:21 -0700

    Broken hovering

--- src/components/Tile/index.jsx
+++ src/components/Tile/index.jsx
@@ -1,7 +1,14 @@
 import './Tile.css';
 import PropTypes from 'prop-types';
 import classNames from "classnames";
+import ReactHover, { Trigger, Hover } from 'react-hover';
 import { shortName } from 'helpers/util';
+
+const optionsCursorTrueWithMargin = {
+  followCursor: true,
+  shiftX: 0,
+  shiftY: 0
+}
 
 // Tile Component
 // Basic display component for a Tile
@@ -11,18 +18,31 @@ const Tile = props => {
   const roundedWeight = Math.round(props.weight * 100000) / 1000;
 
   return (
-    <div 
-      style={props.style} 
-      className={classNames({
-        'tile': true,
-        'done': props.name == 'Done',
-        'working': props.name == shortName('Working on it'),
-        'stuck': props.name == 'Stuck'
-      })}
-      onClick={props.onClick}>
-      <p className='main-title'>Name: {props.name}</p>
-      <p>Value: {props.value}</p>
-      <p>Weight: {roundedWeight}</p>
+    <div onClick={props.onClick}>
+      <ReactHover options={optionsCursorTrueWithMargin}>
+        <Trigger type='trigger'>
+        <div 
+          style={props.style} 
+          className={classNames({
+            'tile': true,
+            'done': props.name == 'Done',
+            'working': props.name == shortName('Working on it'),
+            'stuck': props.name == 'Stuck'
+          })}
+          >
+            <div className='tile-label'>
+              <p className='tile-label-text paragraph-bold'>{props.name}</p>
+            </div>
+          </div>
+        </Trigger>
+        <Hover type='hover'>
+          <div className='tile-info input'>
+            <p>Name: {props.name}</p>
+            <p>Value: {props.value}</p>
+            <p>Weight: {roundedWeight}</p>
+          </div>
+        </Hover>
+      </ReactHover>
     </div>
   );
 };
