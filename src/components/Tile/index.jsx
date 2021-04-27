ee2efe8c502df3730362ac5b9db58d1290b8fa9b
Estevan Sinarta <estevan_sinarta@yahoo.ca>
2021-04-26 21:12:01 -0700

    Fixed conflicts

--- src/components/Tile/index.jsx
+++ src/components/Tile/index.jsx
@@ -36,7 +36,6 @@ const Tile = props => {
       className="tile"
       onClick={props.onClick}
     >
-<<<<<<< HEAD
       {overflowing ? (
         ''
       ) : (
@@ -47,26 +46,6 @@ const Tile = props => {
         </p>
       )}
     </div>
-=======
-      <div
-        id={props.id}
-        ref={tileRef}
-        style={props.style}
-        className="tile"
-        onClick={props.onClick}
-      >
-        {overflowing ? (
-          ''
-        ) : (
-          <p className="tile-label paragraph-bold">
-            {props.name}
-            <br />
-            {props.value}
-          </p>
-        )}
-      </div>
-    </Tooltip>
->>>>>>> f74913b810de05ea93173d8c5dbdc166e5b490a8
   );
 };
 
